f6803d439eebb65d52ba8be90e7838f5ee281fb9
dyleeo <danny@dyleeo.com>
2018-03-31 19:04:19 -0400

    quickfix

--- app/src/index.js
+++ app/src/index.js
@@ -15,7 +15,7 @@ function component() {
     element.appendChild(img);
 
     let btn = document.createElement('button');
-    btn.innerHTML = 'Click me and check the consolezzs!';
+    btn.innerHTML = 'Click me and check the console!';
     btn.onclick = printMe;
 
     element.appendChild(btn);
@@ -33,23 +33,3 @@ if (module.hot) {
     element.parentNode.removeChild(element);
   });
 }
-
-
-// if (module.hot) {
-//   module.hot.accept(function() {
-//      console.log('Accepting the updated printMe module!');
-//      document.body.removeChild(element);
-//      element = component(); // Re-render the "component" to update the click handler
-//      document.body.appendChild(element);
-//   });
-// }
-
-
-// if(module.hot) {
-//   module.hot.accept('./print.js', function() {
-//     console.log('Accepting the updated printMe module!');
-//     document.body.removeChild(element);
-//     element = component(); // Re-render the "component" to update the click handler
-//     document.body.appendChild(element);
-//   })
-// }
